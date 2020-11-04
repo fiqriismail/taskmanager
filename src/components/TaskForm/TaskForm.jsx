20f87b9b058db3440654c8a814f4009b87215256
Fiqri Ismail <fiqri.ismail@gmail.com>
2020-11-04 19:54:23 +0530

    Form done without YUP

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,39 +1,95 @@
 import React from 'react';
+import { useFormik } from 'formik';
+
+const initialValues = {
+  title: '',
+  description: '',
+  dueDate: '',
+};
+
+const onSubmit = (values) => {
+  console.log('form data:', values);
+};
+
+const validate = (values) => {
+  let errors = {};
+  if (!values.title) {
+    errors.title = 'Title is required';
+  }
+
+  if (!values.description) {
+    errors.description = 'Description is required';
+  }
+
+  if (!values.dueDate) {
+    errors.dueDate = 'The due date is required';
+  } else if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(values.dueDate)) {
+    errors.dueDate = 'Date must be dd/mm/yyyy format';
+  }
+  return errors;
+};
 
 function TaskForm() {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validate,
+  });
+
   return (
     <div className="container col-sm-8 mt-4">
       <h4 className="mb-4">Add/Edit Task</h4>
-      <form action="/" method="post">
+      <form onSubmit={formik.handleSubmit}>
         <div className="form-group">
-          <label for="title">Title</label>
+          <label htmlFor="title">Title</label>
           <input
             type="text"
             className="form-control"
             id="title"
+            name="title"
             aria-describedby="emailHelp"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.title}
           />
+          {formik.touched.title && formik.errors.title ? (
+            <div className="small text-danger">{formik.errors.title}</div>
+          ) : null}
         </div>
         <div className="form-group">
-          <label for="description">Description</label>
+          <label htmlFor="description">Description</label>
           <textarea
             rows="4"
             type="text"
             className="form-control"
             id="description"
+            name="description"
             aria-describedby="emailHelp"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            values={formik.values.description}
           />
+          {formik.touched.description && formik.errors.description ? (
+            <div className="small text-danger">{formik.errors.description}</div>
+          ) : null}
         </div>
         <div className="form-group">
-          <label for="dueDate">Due Date</label>
+          <label htmlFor="dueDate">Due Date</label>
           <input
             type="text"
             className="form-control"
             id="dueDate"
+            name="dueDate"
             aria-describedby="emailHelp"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.dueDate}
           />
+          {formik.touched.dueDate && formik.errors.dueDate ? (
+            <div className="small text-danger">{formik.errors.dueDate}</div>
+          ) : null}
         </div>
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" className="btn btn-primary">
           Submit
         </button>
       </form>
