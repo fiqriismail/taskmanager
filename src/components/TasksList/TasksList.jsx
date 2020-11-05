4d16a8672317e7f9ee55528b077bb489071b9cb0
Fiqri Ismail <fiqri.ismail@gmail.com>
2020-11-05 12:50:53 +0530

    Completed with some stylings

--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -5,16 +5,34 @@ import TaskItem from './TaskItem';
 
 function TodoList() {
   const [tasks, setTasks] = useState([]);
+  const [taskUpdated, setTaskUpdated] = useState(false);
 
   useEffect(() => {
     const apiUrl = 'https://reactapidemo-290306.firebaseio.com/tasks.json';
-
     axios.get(apiUrl).then((response) => {
       if (response.data) {
         setTasks(Object.values(response.data));
       }
     });
-  }, []);
+  }, [taskUpdated]);
+
+  const handleComplete = (taskid) => {
+    const apiUrl = `https://reactapidemo-290306.firebaseio.com/tasks/${taskid}.json`;
+    axios.patch(apiUrl, { status: 'Completed' }).then((response) => {
+      if (response.status === 200) {
+        setTaskUpdated(!taskUpdated);
+      }
+    });
+  };
+
+  const handleDelete = (taskid) => {
+    const apiUrl = `https://reactapidemo-290306.firebaseio.com/tasks/${taskid}.json`;
+    axios.delete(apiUrl, { status: 'Completed' }).then((response) => {
+      if (response.status === 200) {
+        setTaskUpdated(!taskUpdated);
+      }
+    });
+  };
 
   const displayTasks = () => {
     if (tasks.length === 0)
@@ -24,13 +42,20 @@ function TodoList() {
         </div>
       );
     return tasks.map((task) => {
-      return <TaskItem key={task.task_id} taskInfo={task} />;
+      return (
+        <TaskItem
+          key={task.task_id}
+          taskInfo={task}
+          onComplete={handleComplete}
+          onDelete={handleDelete}
+        />
+      );
     });
   };
 
   return (
     <div className="container mt-4">
-      <div className="row">{displayTasks()}</div>
+      <div className="row row-cols-1 row-cols-md-3">{displayTasks()}</div>
     </div>
   );
 }
